221cf30cc90898403e45946a579d1ae62e53e332
AsoFC <asofiei.cosmina@yahoo.com>
2017-04-02 11:09:53 +0300

    Tema 3 almost there

--- Tema3/Tema3/Tema3.cpp
+++ Tema3/Tema3/Tema3.cpp
@@ -5,66 +5,89 @@
 #include "gl\glut.h"
 using namespace std;
 
-//#define n 10
-#define SIZE 400
-#define E 0.5
+#define SIZE 500
+#define M 0.5 //margin
 
 unsigned char prevKey;
 
 class GrilaCarteziana {
-	int n = 5;
+	int gridSize = 10;
 public: 
-	void setn(int value) {
-		n = value;
+	void setGridSize(int value) {
+		gridSize = value/2;
 	}
 	void writePixel(int x, int y)
 	{
 		int width = glutGet(GLUT_WINDOW_WIDTH);
 		int height = glutGet(GLUT_WINDOW_HEIGHT);
 		float ratio = float(width) / float(height);
-		glColor3f(0.76, 0.76, 0.76);
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glColor3f(0.5, 0.5, 0.5);
+		glPolygonMode(GL_FRONT, GL_FILL);
 		glBegin(GL_POLYGON);
 		if (ratio < 1) {
 			for (int i = 0; i <= 360; i += 5)
-				glVertex2f((x*(2 - 2 * E) + sin(i)*0.25) / (n + E), ((y*(2 - 2 * E) + cos(i)*0.25) / (n + E))*ratio);
+				glVertex2f((x*(2 - 2 * M) + sin(i)*0.3) / (gridSize + M), ((y*(2 - 2 * M) + cos(i)*0.3) / (gridSize + M))*ratio);
 		}
 		else {
 			ratio = float(height) / float(width);
 			for (int i = 0; i <= 360; i += 5)
-				glVertex2f((x*(2 - 2 * E) + sin(i)*0.25) / (n + E)*ratio, ((y*(2 - 2 * E) + cos(i)*0.25) / (n + E)));
+				glVertex2f((x*(2 - 2 * M) + sin(i)*0.3) / (gridSize + M)*ratio, ((y*(2 - 2 * M) + cos(i)*0.3) / (gridSize + M)));
 		}
 		glEnd();
 	}
 
-	void Pixels(int x, int y, int t, int d)
+	void pixelLine(int x, int y, int t, int d)
 	{
 		t /= 2;
 		if (d == 0)
-			for (int i = -t; i <= t; i++) writePixel(x + i, y);
-		for (int i = -t; i <= t; i++) writePixel(x, y + i);
+			for (int i = -t; i <= t; i++) 
+				if (x+i <= gridSize && x+i >= -gridSize && y <= gridSize && y >= -gridSize)
+					writePixel(x + i, y);
+		for (int i = -t; i <= t; i++) 
+			if (x <= gridSize && x >= -gridSize && y+i <= gridSize && y+i >= -gridSize)
+				writePixel(x, y + i);
 	}
 
-	void AfisareSegmentDreapta3(int fromX, int fromY, int toX, int toY, unsigned int bolded = 0)
+	void AfisareSegmentDreapta3(int x1, int y1, int x2, int y2, unsigned int thickness = 0)
 	{
-		int dx = toX - fromX, x = fromX, y = fromY, dy = toY - fromY;
-		if (fromY < toY) {
-			int d = 2 * dy - dx, dE = 2 * dy, dNE = 2 * (dy - dx);
-			Pixels(x, y, bolded, 1);
-			while (x < toX) {
-				if (d <= 0) { d += dE; x++; }
-				else { d += dNE; x++; y++; }
-				Pixels(x, y, bolded, 1);
+		int dx = x2 - x1, x = x1, y = y1, dy = y2 - y1;
+		if (y1 < y2) {
+			if (x1 < x2) {
+				int d = 2 * dy - dx, dE = 2 * dy, dNE = 2 * (dy - dx);
+					pixelLine(x, y, thickness, 1);
+				while (x < x2) {
+					if (d <= 0) { d += dE; x++; }
+					else { d += dNE; x++; y++; }
+						pixelLine(x, y, thickness, 1);
+				}
+			}
+			else {
+				int dx = x1 - x2, d = 2 * dy - dx, dE = 2 * dy, dNE = 2 * (dy - dx);
+					pixelLine(x, y, thickness, 1);
+				while (x > x2) {
+					if (d <= 0) { d += dE; x--; }
+					else { d += dNE; x--; y++; }
+						pixelLine(x, y, thickness, 1);
+				}
 			}
 		}
 		else {
-			if (toY < fromY) {
-				int dy = fromY - toY, d = 2 * dy - dx, dE = 2 * dy, dNE = 2 * (dy - dx);
-				Pixels(x, y, bolded, 1);
-				while (x < toX) {
+			if (x1 < x2) {
+				int dy = y1 - y2, d = 2 * dy - dx, dE = 2 * dy, dNE = 2 * (dy - dx);
+				pixelLine(x, y, thickness, 1);
+				while (x < x2) {
 					if (d <= 0) { d += dE; x++; }
 					else { d += dNE; x++; y--; }
-					Pixels(x, y, bolded, 1);
+						pixelLine(x, y, thickness, 1);
+				}
+			}
+			else {
+				int dx = x1 - x2, dy = y1 - y2, d = 2 * dy - dx, dE = 2 * dy, dNE = 2 * (dy - dx);
+					pixelLine(x, y, thickness, 1);
+				while (x > x2) {
+					if (d <= 0) { d += dE; x--; }
+					else { d += dNE; x--; y--; }
+						pixelLine(x, y, thickness, 1);
 				}
 			}
 		}
@@ -72,44 +95,45 @@ public:
 
 	void DisplayGrid()
 	{
+		glLineWidth(1.0);
 		int width = glutGet(GLUT_WINDOW_WIDTH);
 		int height = glutGet(GLUT_WINDOW_HEIGHT);
 		float ratio = float(width) / float(height);
 		float i;
 		glColor3f(0.0, 0.0, 0.0);
 		if (ratio < 1) {
-			for (i = -n; i <= n; i++)
+			for (i = -gridSize; i <= gridSize; i++)
 			{
 				glBegin(GL_LINES);
-				glVertex2f((n) / (n + E), (float)(i*ratio / (n + E)));
-				glVertex2f((-n) / (n + E), (float)(i*ratio / (n + E)));
+				glVertex2f((gridSize) / (gridSize + M), (float)(i*ratio / (gridSize + M)));
+				glVertex2f((-gridSize) / (gridSize + M), (float)(i*ratio / (gridSize + M)));
 				glEnd();
 			}
 
-			for (i = -n; i <= n; i++)
+			for (i = -gridSize; i <= gridSize; i++)
 			{
 				glBegin(GL_LINES);
-				glVertex2f((float)(i / (n + E)), (-n)*ratio / (n + E));
-				glVertex2f((float)(i / (n + E)), (n)*ratio / (n + E));
+				glVertex2f((float)(i / (gridSize + M)), (-gridSize)*ratio / (gridSize + M));
+				glVertex2f((float)(i / (gridSize + M)), (gridSize)*ratio / (gridSize + M));
 				glEnd();
 			}
 		}
 		else {
 			ratio = float(height) / float(width);
 
-			for (i = -n; i <= n; i++)
+			for (i = -gridSize; i <= gridSize; i++)
 			{
 				glBegin(GL_LINES);
-				glVertex2f((n)*ratio / (n + E), (float)(i / (n + E)));
-				glVertex2f((-n)*ratio / (n + E), (float)(i / (n + E)));
+				glVertex2f((gridSize)*ratio / (gridSize + M), (float)(i / (gridSize + M)));
+				glVertex2f((-gridSize)*ratio / (gridSize + M), (float)(i / (gridSize + M)));
 				glEnd();
 			}
 
-			for (i = -n; i <= n; i++)
+			for (i = -gridSize; i <= gridSize; i++)
 			{
 				glBegin(GL_LINES);
-				glVertex2f((float)(i*ratio / (n + E)), (-n) / (n + E));
-				glVertex2f((float)(i*ratio / (n + E)), (n) / (n + E));
+				glVertex2f((float)(i*ratio / (gridSize + M)), (-gridSize) / (gridSize + M));
+				glVertex2f((float)(i*ratio / (gridSize + M)), (gridSize) / (gridSize + M));
 				glEnd();
 			}
 		}
@@ -117,6 +141,7 @@ public:
 
 	void DrawLine(int x1, int y1, int x2, int y2, int w)
 	{
+		x1 -= gridSize; x2 -= gridSize; y1 -= gridSize; y2 -= gridSize;
 		int width = glutGet(GLUT_WINDOW_WIDTH);
 		int height = glutGet(GLUT_WINDOW_HEIGHT);
 		float ratio = float(width) / float(height);
@@ -126,15 +151,15 @@ public:
 		glColor3f(1.0, 0.0, 0.0);
 		if (ratio < 1) {
 			glBegin(GL_LINES);
-			glVertex2f(x1 / (n + E), y1*ratio / (n + E));
-			glVertex2f(x2 / (n + E), y2* ratio / (n + E));
+			glVertex2f(x1 / (gridSize + M), y1*ratio / (gridSize + M));
+			glVertex2f(x2 / (gridSize + M), y2* ratio / (gridSize + M));
 			glEnd();
 		}
 		else {
 			ratio = float(height) / float(width);
 			glBegin(GL_LINES);
-			glVertex2f(x1 / (n + E)*ratio, y1 / (n + E));
-			glVertex2f(x2 / (n + E)*ratio, y2 / (n + E));
+			glVertex2f(x1 / (gridSize + M)*ratio, y1 / (gridSize + M));
+			glVertex2f(x2 / (gridSize + M)*ratio, y2 / (gridSize + M));
 			glEnd();
 		}
 
@@ -152,15 +177,25 @@ void Init(void)
 void Display(void)
 {
 	GrilaCarteziana grilaCarteziana;
-	int n = 10;
+	int n = 20;
 	glClear(GL_COLOR_BUFFER_BIT);
 	switch (prevKey)
 	{
 	case '1':
-		grilaCarteziana.setn(n);
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	case '0':
+		grilaCarteziana.setGridSize(n);
 		grilaCarteziana.DisplayGrid();
-		grilaCarteziana.DrawLine(n, 3, -n, n, 3);
-		grilaCarteziana.DrawLine(-n, -n, n, -1, 1);
+		grilaCarteziana.DrawLine(0, n, n, 3*n/4, 3);
+		grilaCarteziana.DrawLine(0, 0, n, n / 2-1, 1);
+		grilaCarteziana.DrawLine(n, n, 0, n / 2, 1);
 		break;
 	default:
 		break;
